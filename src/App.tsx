2ab5e6b1a1baad1775ed89829dab9a6844a5fbc9
opmarq <opmarq@gmail.com>
2021-10-25 12:39:17 +0200

    display notifications & handle markAsRead

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Box, Heading, Container } from "@chakra-ui/react";
+
+import { Card } from './notifications/Card';
+import { getNotifications, INotification, markNotificationAsRead } from "./api/API";
+
 import './App.css';
 
 function App() {
+
+  const [notifications, setNotifications] = useState<INotification[]>([])
+
+  useEffect(() => {
+    getNotifications().then((notifications: any) => {
+      setNotifications(notifications as INotification[])
+    })
+  }, [])
+
+  const handleMarkAsRead = (id: string) => {
+    markNotificationAsRead(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p="1">
+      <Container bgColor="#6c757d" color="white" borderRadius="md" >
+        <Box p="4">
+          <Heading size="md">
+            Notifications
+          </Heading>
+        </Box>
+        <Box>
+          {
+            notifications.map(({ id, image, type, content, description, period, read }) => {
+              return <Card markAsRead={() => {
+                handleMarkAsRead(id)
+              }} read={read} image={image} type={type} content={content} description={description} period={period} />
+            })
+          }
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
---
+++ src/notifications/Card.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Avatar, Flex, Heading, Text } from "@chakra-ui/react";
+
+export interface CardProps {
+	image?: string;
+	type?: string;
+	content?: string;
+	description?: string;
+	period?: string;
+	read: boolean;
+	markAsRead?: () => void
+}
+
+export const Card = ({ image, type, content, description, period, read, markAsRead }: CardProps) => {
+	return <Box _hover={{
+		backgroundColor: "#adb5bd"
+	}} bgColor={read ? "#adb5bd" : 'transparent'} borderTop="1px solid #ced4da" px="4" py="1" cursor="pointer" onMouseEnter={markAsRead} >
+		<Flex>
+			<Box mr="2">
+				<Avatar
+					size="lg"
+					name={description}
+					src={image}
+				/>
+			</Box>
+			<Box>
+				<Heading size="md">{type}</Heading>
+				<Text>{content}</Text>
+			</Box>
+		</Flex>
+		<Text>{period}</Text>
+	</Box>
+}

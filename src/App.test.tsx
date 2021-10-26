09dfabc22a1944b4c330ba921635ca5ed35e491e
opmarq <opmarq@gmail.com>
2021-10-27 00:46:15 +0200

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('display a loader', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loaderElement = screen.getByTitle(/loader/i);
+  expect(loaderElement).toBeDefined()
+});
+
+test('display 20 elements', async () => {
+  render(<App />);
+  await waitFor(() => {
+    const elements = screen.getAllByTitle(/notification-item/i)
+    expect(elements.length).toBe(20)
+  })
+});
+
+test('clear all elements', async () => {
+  render(<App />);
+  const clearButton = screen.getByRole(/clear-all/);
+  expect(clearButton).toBeDefined();
+  fireEvent.click(clearButton);
+  await waitFor(() => {
+    const element = screen.queryByText(/No notifications 👐/i)
+    expect(element).toBeDefined()
+
+  })
 });
